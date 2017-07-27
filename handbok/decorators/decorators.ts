544ce568d113ba7cd0ef3760419a33070578b572
rhb <1041763635@qq.com>
2017-07-27 23:43:08 +0800

    了解decorator2

--- handbok/decorators/decorators.ts
+++ handbok/decorators/decorators.ts
@@ -29,7 +29,7 @@ function sealed(construtor: Function) {
   Object.seal(construtor)
   Object.seal(construtor.prototype)
 }
-
+// class decorator
 @sealed
 class Greeter {
   greeting: string
@@ -41,12 +41,11 @@ class Greeter {
   }
 }
 
-// TODO: go on
 /* 
   commond line
   tsc decorator.ts --target es5 --experimentalDecorator
 */
-
+// method decorator
 class Greeter2 {
   greeting: string
   constructor(message: string) {
@@ -57,8 +56,80 @@ class Greeter2 {
     return `Hello, ${this.greeting}`
   }
 }
-function enumerable(value: boolean) {
+function enumerable(value: boolean) { // this is factory
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     descriptor.enumerable = value
   }
 }
+
+// Accessor decorator
+class Point {
+  private _x: number
+  private _y: number
+  constructor(x: number, y: number) {
+    this._x = x
+    this._y = y
+  }
+
+  @configurable(false)
+  get x() {return this._x}
+
+  @configurable(false)
+  get y() {return this._x}
+}
+
+function configurable(value: boolean) {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.configurable = value
+  }
+}
+function writable(value: boolean) {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.writable = value
+  }
+}
+// property decorator
+
+class Greeter3 {
+  @format("Hello, %s")
+  greeting: string
+
+  constructor(message: string) {
+    this.greeting = message
+  }
+  greet() {
+    let formatString = getFormat(this, 'greeting')
+    return formatString('%s', this.greeting)
+  }
+}
+// TODO:
+// 属性描述器
+
+let o: object, d
+o = {
+  get foo() {
+    return 17
+  }
+}
+d = Object.getOwnPropertyDescriptor(0, 'foo')
+o = { bar: 42}
+d = Object.getOwnPropertyDescriptor(0, 'bar')
+
+// 记住，这些选项不一定是自身属性，如果
+// 是继承来的也要考虑。为了确认保留这些默认值，你可能要
+// 在这之前冻结 Object.prototype，明确指定所有的选项，或者将__prot
+// o__属性指向null。
+
+// // 使用 __proto__
+let  obj = {}
+let descriptor = Object.create(null)
+descriptor.value = 'static'
+Object.defineProperty(obj, 'key', descriptor)
+
+//// 显式
+Object.defineProperty(obj, 'key', {
+   enumerable: false,
+   configurable: false,
+   writable: false,
+    value: "static"
+})
--- handbok/module/module-declaration/main.ts
+++ handbok/module/module-declaration/main.ts
@@ -1,3 +1,6 @@
 /// <reference path="./node.d.ts" />
 import * as URL from 'url'
 let myUrl = URL.parse("http://www.typescriptlang.org")
+
+// 假如你不想在使用一个新模块之前花时间去编写声明，你可以采用声明的简写形式以便能够快速使用它。
+// declare module "hot-new-module"
